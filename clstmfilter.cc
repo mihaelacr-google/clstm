2c454b2dd0c32c027c8cb0889dceb36c10432c68
Mihaela Rosca <mihaelacr@google.com>
2016-11-19 14:03:09 +0100

    Add option to get the CER from a loaded model.

--- clstmfilter.cc
+++ clstmfilter.cc
@@ -33,7 +33,61 @@ using std_wstring = std::wstring;
 #define string std_string
 #define wstring std_wstring
 
-int main1(int argc, char **argv) {
+struct Sample {
+  wstring in, out;
+};
+
+void read_samples(vector<Sample> &samples, const string &fname) {
+  ifstream stream(fname);
+  string line;
+  wstring in, out;
+  samples.clear();
+  while (getline(stream, line)) {
+    // skip blank lines and lines starting with a comment
+    if (line.substr(0, 1) == "#") continue;
+    if (line.size() == 0) continue;
+    int where = line.find("\t");
+    if (where < 0) THROW("no tab found in input line");
+    in = utf8_to_utf32(line.substr(0, where));
+    out = utf8_to_utf32(line.substr(where + 1));
+    if (in.size() == 0) continue;
+    if (out.size() == 0) continue;
+    samples.push_back(Sample{in, out});
+  }
+}
+
+int main_with_gt(int argc, char **argv) {
+  string load_name = getsenv("load", "");
+  if (load_name == "") THROW("must give load= parameter");
+  CLSTMText clstm;
+  clstm.load(load_name);
+
+  vector<Sample> test_samples;
+  read_samples(test_samples, argv[1]);
+
+  int ch_count = 0;
+  int ch_errors = 0;
+
+  double exact_matches = 0.0;
+  for (int test = 0; test < test_samples.size(); test++) {
+    wstring gt = test_samples[test].out;
+    wstring pred = clstm.predict(test_samples[test].in);
+    ch_count += gt.size();
+    ch_errors += levenshtein(pred, gt);
+    if (pred == gt) {
+      exact_matches++;
+    }
+    cout << gt << "\t" << pred << endl;
+  }
+  double exact_test_error = 1.0 - exact_matches * 1.0 / test_samples.size();
+  double ch_test_error = ch_errors * 1.0 / ch_count;
+  print("ERROR", ch_test_error, " ", ch_errors, ch_count,
+        "exact_test_error", exact_test_error);
+  return 0;
+}
+
+
+int main_without_gt(int argc, char **argv) {
   if (argc != 2) THROW("give text file as an argument");
   const char *fname = argv[1];
 
@@ -60,12 +114,23 @@ int main1(int argc, char **argv) {
   return 0;
 }
 
+
 int main(int argc, char **argv) {
+  int with_gt = getienv("with_gt", 0);
+
 #ifdef NOEXCEPTION
-  return main1(argc, argv);
+  if (with_gt) {
+    return main_with_gt(argc, argv);
+  } else {
+    return main_without_gt(argc, argv);
+  }
 #else
   try {
-    return main1(argc, argv);
+  if (with_gt) {
+      return main_with_gt(argc, argv);
+    } else {
+      return main_without_gt(argc, argv);
+    }
   } catch (const char *message) {
     cerr << "FATAL: " << message << endl;
   }
